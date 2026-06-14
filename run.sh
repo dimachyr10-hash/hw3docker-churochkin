b4e77a45bbee61a1b549713a7bf9e4e3ee83ded1
dimachyr10-hash <dimachyr10@gmail.com>
2026-06-14 22:46:00 +0000

    Дописал команды в баш скрипт, для очистки папки со сгенерированнми данными и аналитики, для описания структуры проекта, и две для запуска контейнеров чтобы посмотреть что они видят содержимое папки дата

--- run.sh
+++ run.sh
@@ -21,6 +21,26 @@ case "$1" in
   run_reporter)
     docker run --rm -v "$(pwd)/data:/data" football-reporter
     ;;
+
+  structure)
+    echo "Project structure:"
+    find . -maxdepth 3 -type f | sort
+    ;;
+
+  clear_data)
+    rm -f data/*.csv
+    rm -f data/*.html
+    rm -f local_data/*.csv
+    echo "Data files were removed"
+    ;;
+
+  inside_generator)
+    docker run --rm -it -v "$(pwd)/data:/data" --entrypoint sh football-generator
+    ;;
+
+  inside_reporter)
+    docker run --rm -it -v "$(pwd)/data:/data" --entrypoint sh football-reporter
+    ;;
     
   *)
     echo "Unknown command"
@@ -30,5 +50,9 @@ case "$1" in
     echo "  create_local_data"
     echo "  build_reporter"
     echo "  run_reporter"
+    echo "  structure"
+    echo "  clear_data"
+    echo "  inside_generator"
+    echo "  inside_reporter"
     ;;
 esac
