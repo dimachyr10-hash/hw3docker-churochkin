098ddb5d8fcf9f0b9519d9554e86452c153671e9
dimachyr10-hash <dimachyr10@gmail.com>
2026-06-14 22:15:59 +0000

    Добавил в баш скрипт каоманды билд и ран для аналитика, также файл который соторый создаётся по итогу сборки закинул в гит игнор

--- run.sh
+++ run.sh
@@ -9,15 +9,26 @@ case "$1" in
     docker run --rm -v "$(pwd)/data:/data" football-generator
     ;;
 
-   create_local_data)
+  create_local_data)
     mkdir -p local_data
     python generator/generate.py local_data
     ;;
 
+  build_reporter)
+    docker build -t football-reporter ./reporter
+    ;;
+
+  run_reporter)
+    docker run --rm -v "$(pwd)/data:/data" football-reporter
+    ;;
+    
   *)
     echo "Unknown command"
     echo "Available commands:"
     echo "  build_generator"
     echo "  run_generator"
+    echo "  create_local_data"
+    echo "  build_reporter"
+    echo "  run_reporter"
     ;;
 esac
